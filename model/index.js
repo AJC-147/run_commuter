d03fd4ef857f11328d4040725f1f6a827b6f1748
alexander.chacko <alexander.chacko@outlook.com>
2018-10-09 15:09:23 -0400

    file structure3

--- model/index.js
+++ model/index.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-"use strict";
-
-var fs = require("fs");
-var path = require("path");
-var Sequelize = require("sequelize");
-var basename = path.basename(__filename);
-var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.json")[env];
-var db = {};
-
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-=======
 'use strict';
 
 const fs = require('fs');
@@ -29,23 +13,15 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
->>>>>>> 70a8572c42cbffe0d73e4f70f2851fa08960e703
 }
 
 fs
   .readdirSync(__dirname)
   .filter(file => {
-<<<<<<< HEAD
-    return (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js");
-  })
-  .forEach(file => {
-    var model = sequelize["import"](path.join(__dirname, file));
-=======
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
     const model = sequelize['import'](path.join(__dirname, file));
->>>>>>> 70a8572c42cbffe0d73e4f70f2851fa08960e703
     db[model.name] = model;
   });
 
