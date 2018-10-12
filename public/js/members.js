9ccce573e23724b6ffeabb2c8d61b2523fb74fd5
alexander.chacko <alexander.chacko@outlook.com>
2018-10-12 11:25:09 -0400

    minor

--- public/js/members.js
+++ public/js/members.js
@@ -1,6 +1,6 @@
-$(document).ready(function() {
-    console.log(req.session.passport.user)
-  $.get("/api/user_data").then(function(data) {
-    $(".member-name").text(data.email);
-  });
-});
+//$(document).ready(function() {
+//    console.log(req.session.passport.user)
+//  $.get("/api/user_data").then(function(data) {
+//    $(".member-name").text(data.email);
+//  });
+//});
